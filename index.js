8880fd9beb18cc2a527255261b0d4200de3a96f2
Alessandra Chardon <achardon@ucla.edu>
2021-08-30 15:17:22 -0400

    commit tree ec014ba10148f48a9b74233edaa71a0e97c54d6b

--- index.js
+++ index.js
@@ -1,1 +1,11 @@
 // Write your code in this file!
+
+const currentUser = 'Lorenzo';
+
+const welcomeMessage = 'Welcome to Flatbook, ' + currentUser + " !";
+
+const excitedWelcomeMessage = welcomeMessage.toUpperCase();
+
+const userInitial = currentUser.slice(0, 1);
+
+const shortGreeting = `Welcome, ${userInitial}!`;
